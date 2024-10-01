856538c69ae909f6b0d1defcb3bff47a53aeeb4b
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-10-01 20:46:03 +0300

    add text field to add note bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -8,23 +8,46 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          Text(
-            'Add Note',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueGrey[100],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              'Add Note',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueGrey[100],
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
-          const CustomTextField(
-            hintText: 'Enter your note',
-          ),
-        ],
+            const SizedBox(height: 16),
+            const CustomTextField(
+              hintText: 'Enter your note title here...',
+              maxLines: 1,
+            ),
+            const SizedBox(height: 16),
+            const CustomTextField(
+              hintText: 'Enter your note content here...',
+              maxLines: 5,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueGrey[100],
+              ),
+              child: const Text(
+                'Add Note',
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText});
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.maxLines = 1,
+  });
   final String hintText;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxLines,
       cursorColor: Colors.blueGrey,
       decoration: InputDecoration(
         border: buildBorder(),

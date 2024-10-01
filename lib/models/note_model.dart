cc958894f4c1db808c18e770486495abf99fca13
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-10-01 22:09:03 +0300

    Creates notes view model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String color;
+  final String createdAt;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.color,
+    required this.createdAt,
+  });
+}

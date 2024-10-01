b41c5a286ecf17f4f58fb627fba008d75e97b577
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-10-01 22:02:06 +0300

    Edit notes view done

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 4,
+        horizontal: 24,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(height: 16),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 10,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

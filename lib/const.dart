e8e720a335a98bae7c51981604fc63af69e61a1b
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-10-03 04:37:26 +0300

    Init Hive

---
+++ lib/const.dart
@@ -0,0 +1,1 @@
+const String kNotsBox = "nots_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/const.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
-  runApp(const NotesApp());
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotsBox);
+  runApp(
+    const NotesApp(),
+  );
 }
 
 class NotesApp extends StatelessWidget {

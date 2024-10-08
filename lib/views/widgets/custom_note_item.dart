db14e86a3e556b30c1bd931e4c980bc681127cc9
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-10-08 22:46:31 +0300

    Delete note

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -63,7 +63,9 @@ class CustomNoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
